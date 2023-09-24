2c1bafc4d09a7026838b2a168972d0f3d8fe001f
sara latorre <saralatorre97@gmail.com>
2023-09-24 22:45:45 +0200

    added function to get contacts by id

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -1,16 +1,34 @@
-const { getDb } = require("../db/connect");
-const ObjectId = require('mongodb').ObjectId;
+const mongodb = require('../db/connect');
+const ObjectId = require("mongodb").ObjectId; 
 
-const getContacts = async (req, res, next) => {
+// get all contacts
+const getAll = async (req, res, next) => {
   try {
-    const db = getDb();
-    const contacts = await db.collection("contacts").find({}).toArray();
-    res.json(contacts);
-  } catch (err) {
-    console.error(err);
+    const result = await mongodb.getDb().db("contacts").collection("contacts").find().toArray();
+
+    // set the response content type to JSON and send the result
+    res.setHeader("Content-Type", "application/json");
+    res.status(200).json(result);
+  } catch (error) {
+    console.error(error);
   }
 };
 
-module.exports = {
-  getContacts
+//get a single contact by ID
+const getSingle = async (req, res, next) => {
+  const userId = new ObjectId(req.params.id); // create an ObjectId from the request parameter
+
+  try {
+    const result = await mongodb.getDb().db("contacts").collection("contacts").find({ _id: userId });
+
+    // convert the result to an array and send the first item
+    result.toArray().then((lists) => {
+      res.setHeader("Content-Type", "application/json");
+      res.status(200).json(lists[0]);
+    });
+  } catch (error) {
+    console.error(error);
+  }
 };
+
+module.exports = { getAll, getSingle };
