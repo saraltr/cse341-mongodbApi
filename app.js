84603bc96731e43316fcf2f6e01ec98aece0adb2
sara latorre <saralatorre97@gmail.com>
2023-10-07 08:59:31 +0200

    added frontend link

--- app.js
+++ app.js
@@ -17,7 +17,11 @@ app
 .use(express.json())
 .use(cors()) 
 .use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Origin", "https://cse341-contacts-frontend.netlify.app");
+  res.setHeader("Access-Control-Allow-headers", 
+  "Origin, X-Requested-With, Content-Type, Accept, Z-Key");
+  res.setHeader("Content-Type", 
+  "application/json");
   next();
 })
 .use("/", require("./routes"))
